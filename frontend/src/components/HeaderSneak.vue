24748ffaf81235448dcc2cedf0678bf871604bfc
danya <danyakresichenko0709@gmail.com>
2024-06-05 02:01:09 +0300

    front_better

--- frontend/src/components/HeaderSneak.vue
+++ frontend/src/components/HeaderSneak.vue
@@ -1,10 +1,10 @@
 <template>
     <header class="flex justify-between border-b border-slate-200 px-10 py-8">
       <div class="flex items-center gap-4">
-        <img src="/logo.jpeg" alt="Logo" class="w-10">
+        <img src="/logo.png" alt="Logo" class="w-10">
         <div>
-          <h2 class="text-xl font-bold uppercase">Барыги Даня и Карим</h2>
-          <p class="text-slate-500">Магазин поношенных кроссовок</p>
+          <h2 class="text-xl font-bold uppercase">Sneaker catalog</h2>
+          <p class="text-slate-500">Протираем подошву</p>
         </div>
       </div>
 
